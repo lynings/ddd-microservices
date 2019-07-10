02248cd5091ade0f53d9121d4a5819b5164b73b3
lyning <645464704@qq.com>
2019-07-11 02:46:53 +0800

    使用 webflux 替代 webmvc

--- ordercontext/src/main/java/pers/lyning/springcloud/ordercontext/OrderContextApplication.java
+++ ordercontext/src/main/java/pers/lyning/springcloud/ordercontext/OrderContextApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
  * @author lyning
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableWebFlux
 public class OrderContextApplication {
 
     public static void main(final String[] args) {
--- usercontext/src/main/java/pers/lyning/springcloud/usercontext/UserContextApplication.java
+++ usercontext/src/main/java/pers/lyning/springcloud/usercontext/UserContextApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
  * @author lyning
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableWebFlux
 public class UserContextApplication {
 
     public static void main(final String[] args) {
