90ada48e2b92605d8a57a6463d18760e96db09c0
lyning <645464704@qq.com>
2019-07-11 02:47:06 +0800

    删除多余代码

--- infra-gateway/src/main/java/pers/lyning/springcloud/gateway/GatewayApplication.java
+++ infra-gateway/src/main/java/pers/lyning/springcloud/gateway/GatewayApplication.java
@@ -3,8 +3,6 @@ package pers.lyning.springcloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
 
 /**
  * @author lyning
@@ -15,10 +13,5 @@ public class GatewayApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Bean
-    public ServerCodecConfigurer serverCodecConfigurer() {
-        return ServerCodecConfigurer.create();
     }
 }
