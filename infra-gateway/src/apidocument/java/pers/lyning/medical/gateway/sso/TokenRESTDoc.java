3ba2af4a0168bfffd9306da7d2b2b4147e58eee0
lyning <645464704@qq.com>
2019-07-16 12:01:52 +0800

    修改 spring rest doc 文档

--- infra-gateway/src/apidocument/java/pers/lyning/medical/gateway/sso/TokenRESTDoc.java
+++ infra-gateway/src/apidocument/java/pers/lyning/medical/gateway/sso/TokenRESTDoc.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -12,6 +13,8 @@ import pers.lyning.medical.gateway.client.patient.Patient;
 import pers.lyning.medical.gateway.client.patient.PatientClient;
 import pers.lyning.medical.gateway.client.patient.Permission;
 import pers.lyning.medical.gateway.oss.ApplyTokenCommand;
+import pers.lyning.medical.gateway.oss.JwtProvider;
+import pers.lyning.medical.gateway.oss.Payload;
 import pers.lyning.medical.gateway.oss.Token;
 import pers.lyning.medical.gateway.testing.BaseIntegrationTest;
 
@@ -21,13 +24,16 @@ import java.util.ArrayList;
  * @author lyning
  */
 @WebFluxTest
-class TokenDoc extends BaseIntegrationTest {
+class TokenApiDoc extends BaseIntegrationTest {
 
+    public static final String URI = "/tokens";
     @MockBean
     private PatientClient patientClientMock;
+    @SpyBean
+    private JwtProvider jwtProvider;
 
     @Test
-    void applyToken() throws Exception {
+    void apply() throws Exception {
         // given
         final String username = "admin";
         final String password = "admin";
@@ -37,16 +43,45 @@ class TokenDoc extends BaseIntegrationTest {
         BDDMockito.doReturn(patient).when(this.patientClientMock).obtainByUsername(username);
         // when
         final WebTestClient.ResponseSpec responseSpec = this.client.post()
-                .uri("/tokens")
+                .uri(URI)
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .body(BodyInserters.fromObject(new ApplyTokenCommand(username, password)))
                 .exchange();
         // then
-        final Token expectedToken = Token.fillPrefix("eyJhbGciOiJIUzUxMiJ9.eyJwZXJtaXNzaW9ucyI6W3sidXJsIjoiL3BhdGllbnRzIiwicmVzb3VyY2UiOiJsaW5rLmdldC51c2VyIiwibWV0aG9kIjoiZ2V0In1dLCJuYW1lIjoiYWRtaW4iLCJ1c2VybmFtZSI6ImFkbWluIn0.TKrY-oYgXslajPDblH5AwuCZtw7SCnG4dwJzMoLeaNve-7pMugSUu7we4uAFvXRzqKM_oK_r297lwnjfaGJdjg");
         responseSpec.expectStatus().is2xxSuccessful()
                 .expectBody(Token.class)
-                .isEqualTo(expectedToken)
+                .value((token -> token.getToken().startsWith("Bearer eyJhbGciOiJIUzUxMiJ9")))
                 .consumeWith(this.commonDocumentation());
+    }
+
+    @Test
+    void renew() throws Exception {
+        // given
+        final Token token = this.generateUnexpiredToken();
+        // when
+        final WebTestClient.ResponseSpec responseSpec = this.client
+                .post()
+                .uri(URI)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .body(BodyInserters.fromObject(token))
+                .exchange();
+
+        // then
+        responseSpec.expectBody(Token.class)
+                .value((jwt -> jwt.getToken().startsWith("Bearer eyJhbGciOiJIUzUxMiJ9")))
+                .consumeWith(this.commonDocumentation());
+    }
+
+
+    private Token generateUnexpiredToken() {
+        Payload payload = preparePayload();
+        return this.jwtProvider.generate(payload);
+    }
+
+    private Payload preparePayload() {
+        final ArrayList<Permission> permissions = Lists.newArrayList(new Permission("/patients", "link.get.user", "get"));
+        final Patient patient = new Patient("admin", "admin", permissions);
+        return new Payload(patient);
     }
 
 }
