c8b1f7b3e49df82ec40d0a91081689cb644cc15a
lyning <645464704@qq.com>
2019-07-22 08:54:27 +0800

    增加 序列号id

--- infra-gateway/src/main/java/pers/lyning/medical/gateway/exception/AuthenticationException.java
+++ infra-gateway/src/main/java/pers/lyning/medical/gateway/exception/AuthenticationException.java
@@ -8,15 +8,17 @@ import org.springframework.web.server.ResponseStatusException;
  */
 public class AuthenticationException extends ResponseStatusException {
 
+    private static final long serialVersionUID = -3913080873451211952L;
+
     private AuthenticationException(final HttpStatus httpStatus, final String errorMsg) {
         super(httpStatus, errorMsg);
     }
 
-    public static AuthenticationException throwUnauthorized() {
-        return new AuthenticationException(HttpStatus.UNAUTHORIZED, "未认证");
-    }
-
     public static AuthenticationException throwForbidden() {
         return new AuthenticationException(HttpStatus.FORBIDDEN, "认证失败");
+    }
+
+    public static AuthenticationException throwUnauthorized() {
+        return new AuthenticationException(HttpStatus.UNAUTHORIZED, "未认证");
     }
 }
